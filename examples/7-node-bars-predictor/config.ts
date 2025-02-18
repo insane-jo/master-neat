f28f1187e8b69c76ed59375b61a3a4d8a23b9a49
aleksei <hitechrussia@gmail.com>
2025-02-18 08:33:55 +0300

    Additional info on iterations

--- examples/7-node-bars-predictor/config.ts
+++ examples/7-node-bars-predictor/config.ts
@@ -54,7 +54,7 @@ export const DRAW_RESULTS_CALLBACK = (startDate: number, TEST_SET: PointData[]) 
 
     const testError = totalTest.error.toFixed(10);
 
-    console.log(`Iteration: ${iterationTest}, train error: ${results.error.toFixed(10)}. Test error: ${testError}. Predictions: (+${correct})|(-${incorrect})/${total} Network configuration: ${networkConfiguration}.`)
+    console.log(`Iteration: ${iterationTest}, train error: ${results.error.toFixed(10)}. Test error: ${testError}. Predictions: (+${correct})|(-${incorrect})/${total} (${(correct/total * 100).toFixed(2)}%/${(incorrect/total * 100).toFixed(2)}%) Network configuration: ${networkConfiguration}.`)
 
     if (results.iteration % SAVE_NETWORK_ITERATIONS == 0) {
       fs.writeFile(
