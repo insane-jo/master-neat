d8277c37f26d710dc0c7ccb86a14bbb8402fd3ef
aleksei <hitechrussia@gmail.com>
2025-02-19 10:37:06 +0300

    Performance fix for results

--- examples/7-node-bars-predictor/config.ts
+++ examples/7-node-bars-predictor/config.ts
@@ -229,12 +229,12 @@ export const DRAW_RESULTS_CALLBACK = (startDate: number, TEST_SET: PointData[]) 
 
     let correct = 0,
       incorrect = 0,
-      total = TEST_SET.length
+      total = TEST_SET.length;
 
-    for(let i = 0, l = total; i < l; i++) {
-      const testItem = TEST_SET[i];
+    if (NETWORK_OUTPUT_AMOUNT === 2) {
+      for (let i = 0, l = total; i < l; i++) {
+        const testItem = TEST_SET[i];
 
-      if (NETWORK_OUTPUT_AMOUNT === 2) {
         const [correctCategory1, correctCategory2] = testItem.output;
 
         const result = bestNetwork.activate(testItem.input);
@@ -247,7 +247,11 @@ export const DRAW_RESULTS_CALLBACK = (startDate: number, TEST_SET: PointData[]) 
         } else {
           incorrect++;
         }
-      } else {
+      }
+    } else {
+      for (let i = 0, l = total; i < l; i++) {
+        const testItem = TEST_SET[i];
+
         const [correctCategory] = testItem.output;
         const [result] = bestNetwork.activate(testItem.input);
 
@@ -265,13 +269,14 @@ export const DRAW_RESULTS_CALLBACK = (startDate: number, TEST_SET: PointData[]) 
 
     const testError = totalTest.error.toFixed(10);
 
-    console.log(`Iteration: ${iterationTest}, train error: ${results.error.toFixed(10)}. Test error: ${testError}. Predictions: (+${correct})|(-${incorrect})/${total} (${(correct/total * 100).toFixed(2)}%/${(incorrect/total * 100).toFixed(2)}%) Network configuration: ${networkConfiguration}.`)
+    console.log(`Iteration: ${iterationTest}, train error: ${results.error.toFixed(10)}. Test error: ${testError}. Predictions: (+${correct})|(-${incorrect})/${total} (${(correct / total * 100).toFixed(2)}%/${(incorrect / total * 100).toFixed(2)}%) Network configuration: ${networkConfiguration}.`)
 
     if (results.iteration % SAVE_NETWORK_ITERATIONS == 0) {
       fs.writeFile(
         EXPORT_FILENAME,
         JSON.stringify(bestNetwork),
-        () => {}
+        () => {
+        }
       );
     }
   };
